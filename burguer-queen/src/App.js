ef464f840bb4124f5f747545309afefd5c2d06a9
Talia Gonzalez <triptofanita@Talias-MacBook-Pro.local>
2021-04-28 19:16:16 -0500

    prueba instalacion react

--- burguer-queen/src/App.js
+++ burguer-queen/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        hello world branch talia
+        hello world!!! <3
         </p>
         <a
           className="App-link"
